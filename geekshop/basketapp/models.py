66d6c93fb5d0ec9477296437c9b8672990f46bc3
kindlyhickory <kindlyhickory@gmail.com>
2020-11-10 18:29:54 +0300

    cached_property2

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -40,13 +40,13 @@ class Basket(models.Model):
 
     @property
     def total_quantity(self):
-        _items = self.get_items_cached()
+        _items = self.get_items_cached
         _total_quantity = sum(list(map(lambda x: x.quantity, _items)))
         return _total_quantity
 
     @property
     def total_cost(self):
-        _items = self.get_items_cached()
+        _items = self.get_items_cached
         _total_cost = sum(list(map(lambda x: x.product_cost, _items)))
         return _total_cost
     @staticmethod
