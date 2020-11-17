22484abba213fa241ac5e12b128170e33958f2bb
kindlyhickory <kindlyhickory@gmail.com>
2020-11-17 19:11:15 +0300

    fixes

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -8,11 +8,11 @@ from django.utils.timezone import now
 
 
 class ShopUser(AbstractUser):
-    avatar = models.ImageField(upload_to='users_avatars', blank=True, verbose_name='аватарка')
+    avatar = models.ImageField(upload_to='users_avatars', blank=True, verbose_name='аватарка', max_length=255)
     age = models.PositiveIntegerField(verbose_name='возраст', default=18)
     is_active = models.BooleanField(default=True)
 
-    activation_key = models.CharField(blank=True)
+    activation_key = models.CharField(max_length=128, blank=True)
     activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
 
     def is_activation_key_expired(self):
