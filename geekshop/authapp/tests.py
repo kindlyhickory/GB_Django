b4d0e0ef7a396f0baa7b0bfa44d35657aeee44ad
kindlyhickory <kindlyhickory@gmail.com>
2020-11-20 18:23:05 +0300

    test_products methods

--- geekshop/authapp/tests.py
+++ geekshop/authapp/tests.py
@@ -13,3 +13,17 @@ class TestUserManagement(TestCase):
         self.assertEqual(list(response.context['basket']), [])
         self.assertEqual(response.request['PATH_INFO'], '/basket/')
         self.assertIn('Ваша корзина, Пользователь', response.content.decode())
+
+    def test_user_logout(self):
+        self.client.login(username='tarantino', password='geekbrains')
+
+        response = self.client.get('/auth/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['user'].is_anonymous)
+
+        response = self.client.get('/auth/logout/')
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_anonymous)
